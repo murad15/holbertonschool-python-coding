8ef5ea0b6239a239bfe49af774bd5dbe62dec159
murad15 <muradis1503@gmail.com>
2025-12-12 07:06:27 +0000

    q

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Represent a square defined by its size."""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the area size of square"""
+        area = self.__size**2
+        return area
