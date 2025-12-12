42d2aaf96c9654749cefa2e603f8e7c4f7ed0d43
murad15 <muradis1503@gmail.com>
2025-12-12 06:28:34 +0000

    q

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute."""
+
+class Square:
+    """Represent a square defined by its size."""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance with a given size."""
+        self.__size = size
